39ad7dcb423a545a8787028ae56872d51a33e157
Alexis <alexisvallini@gmail.com>
2019-07-16 22:56:32 +0200

    MAJ 16.07

--- GeoFrance/mysql.js
+++ GeoFrance/mysql.js
@@ -37,15 +37,3 @@ app.get('/', HomePage);
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
-
-  /*var sql = "Select Region.NomRegion, Ville.NomVille FROM Region INNER JOIN Ville ON Ville.IdRegion=Region.IdRegion Where Ville.ChefLieuRegion IS TRUE";
-  console.log("SQL=" + sql);
-
-  conn.query(sql, function (err, rows, fields) {
-    if (err) throw err;
-    for (var i = 0; i < rows.length; i++) {
-      console.log("  - Nom de la Région : " + rows[i].NomRegion);
-      console.log("  - Nom de la Ville : " + rows[i].NomVille);
-      console.log("\n");
-    }
-  });*/
